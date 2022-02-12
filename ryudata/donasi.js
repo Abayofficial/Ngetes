685694c57913bfb682af640573813fc27ecccce0
Abayofficial <iqbalantariksa9@gmail.com>
2022-02-12 22:09:53 +0700

    Ngetes

---
+++ ryudata/donasi.js
@@ -0,0 +1,28 @@
+//EDIT SESUKA KALIAN 😉
+
+// UCAPAN WELCOME
+exports.donasi = () => {
+	return`*TERIMA KASIH SUDAH MAU DONASI 🤗*
+	
+	╔════════════•| ✿ |•═════════════╗
+                                *_PAYMENT_*:
+
+➪ *_GOPAY_*: 087788069091  A/N ABAY STORE
+➪ *_DANA_*: 087788069091 A/N RE** HER**** YAN**
+
+                      *_SERTAKAN BUKTI TF!!_*
+╚════════════•| ✿ |•═════════════╝
+
+*Q.S Az-Zalzalah Ayat 7 :*
+
+فَمَنْ يَّعْمَلْ مِثْقَالَ ذَرَّةٍ خَيْرًا يَّرَهٗۚ
+
+Artinya : Maka barangsiapa 
+mengerjakan kebaikan seberat
+zarrah, niscaya dia akan melihat 
+(balasan)nya,
+
+contact owner :
+wa.me/6287788069091 (Owner)
+`
+}
---
+++ ryudata/sewa.js
@@ -0,0 +1,23 @@
+exports.sewa = () => {
+	return`🇸 🇪 🇼 🇦  🇧 🇴 🇹 ❗
+
+┏━━━━━━
+┃𝗛𝗔𝗥𝗚𝗔 𝗦𝗘𝗪𝗔 :
+┃ *PERMINGGU : Rp 5.000*
+┃ *PERBULAN : Rp 7.000*
+┃ *PERMANEN : Rp 10.000*
+┗━━━━━━
+┏━━━━━━
+┃𝗙𝗜𝗧𝗨𝗥 𝗕𝗢𝗧 :
+┃ *⚔️ Antilink Group*
+┃ *👨‍🎨 Buat Stiker*
+┃ *📥 Downloader*
+┃ *Dll.*
+┗━━━━━━
+
+*SISTEM: TES BOT-MASUKIN BOT-TF-DONE*
+
+*MINAT? CHAT :
+wa.me/6287788069091
+`
+}
